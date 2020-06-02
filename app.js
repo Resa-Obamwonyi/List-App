7307c0d440afef9643438f57098d5ae27ffdf48a
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-06-02 23:13:18 +0100

    new items can now be added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -20,6 +21,25 @@ app.get('/index', (req, res) => {
       res.render('index.ejs', {items: results});
     }
   );
+});
+
+app.get('/add',(req,res) => {
+  res.render('add.ejs')
+});
+
+
+app.post('/create', (req, res) => {
+  // Write a query to add data to the database
+  connection.query( 'INSERT INTO items (name) VALUES (?) ', [req.body.itemName], 
+  (error, results)=>{
+      connection.query(
+  // Add a route method for creating items
+        'SELECT * FROM items',
+        (error, results) => {
+          res.render('index.ejs', {items: results});
+        }
+      );
+  });
 });
 
 app.listen(3000);
