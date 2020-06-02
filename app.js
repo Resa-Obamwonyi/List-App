3ae1b4042231fef23b46b3b4bb54049bb7e7df9a
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-06-02 16:29:55 +0100

    lists are displayed from database

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/index', (req,res) => {
-    res.render('index.ejs');
-  });
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'list_app'
+});
+
+app.get('/index', (req, res) => {
+  // Write the necessary code to get data from the database
+  connection.query(
+    'SELECT * FROM items',
+    (error,results)=>{
+      console.log(results);
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
 
 app.listen(3000);
