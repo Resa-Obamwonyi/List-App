903b4a0bb657eb503fbe225771e7545bc815a6d9
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-06-02 23:50:05 +0100

    redirect set up

--- app.js
+++ app.js
@@ -32,14 +32,11 @@ app.post('/create', (req, res) => {
   // Write a query to add data to the database
   connection.query( 'INSERT INTO items (name) VALUES (?) ', [req.body.itemName], 
   (error, results)=>{
-      connection.query(
-  // Add a route method for creating items
-        'SELECT * FROM items',
-        (error, results) => {
-          res.render('index.ejs', {items: results});
-        }
-      );
-  });
+  // Add a route method for redirecting to the items    
+    res.redirect('/index')
+      
+  }
+  );
 });
 
 app.listen(3000);
